155bda5e2de6cb76324e6bb0c81ef3780e4e1773
Yusuf <yusuf2002kh@gmail.com>
2023-07-03 00:20:36 +0500

    a

--- src/helpers/const.jsx
+++ src/helpers/const.jsx
@@ -19,11 +19,11 @@ export const portfolioFirst = [
 export const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 3000 },
-    items: 3,
+    items: 4,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 4,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
